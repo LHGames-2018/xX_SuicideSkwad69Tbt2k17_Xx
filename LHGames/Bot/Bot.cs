a47c43859b41840ff9a36aa1243d3a33e91c2726
Jonathan Bordeleau <jonathan.bordeleau@outlook.com>
2018-10-06 15:25:28 -0400

    New algo for pathfinding

--- LHGames/Bot/Bot.cs
+++ LHGames/Bot/Bot.cs
@@ -118,21 +118,13 @@ namespace LHGames.Bot
         /// </summary>
         public static class MovementActions
         {
-            //public MovementActions(Map map)
-            //{
-            //    MapInstance = map;
-            //}
-
-            //Map MapInstance { get; set; }
-
             /// <summary>
             /// Used by the other classes to specify a point where we need to move
-            /// ie: the player wan't to move to a point positioned 1 right, 3 up, MoveTo(new Point(player.X + 1, Player.Y - 3));
+            /// ie: the player wan't to move to a point positioned 1 right & 3 up, point should be [1, -3]
             /// </summary>
-            /// <param name="point">The point where the player wants to end up</param>
+            /// <param name="point"></param>
             public static string MoveTo(Map map, Point point)
             {
-                //List<Point> path = FindPath(map, point);
                 return FindEasyPath(map, point);
             }
 
@@ -143,49 +135,123 @@ namespace LHGames.Bot
                      moveUp = false,
                      moveDown = false;
 
-                int nbMovesX = point.X;
-                int nbMovesY = point.Y;
+                if (point.X > 0)
+                {
+                    if(map.GetTileAt(PlayerInfo.Position.X + 1, PlayerInfo.Position.Y) != TileContent.Lava)
+                    {
+                        moveRight = true;
+                    }
+                    else
+                    {
+                        if(map.GetTileAt(PlayerInfo.Position.X, PlayerInfo.Position.Y - 1) != TileContent.Lava)
+                        {
+                            return AIHelper.CreateMoveAction(new Point(0, -1));
+                        }
+                        else if(map.GetTileAt(PlayerInfo.Position.X, PlayerInfo.Position.Y + 1) != TileContent.Lava)
+                        {
+                            return AIHelper.CreateMoveAction(new Point(0, 1));
+                        }
+                    }
+                }
+                else if (point.X < 0)
+                {
+                    if (map.GetTileAt(PlayerInfo.Position.X - 1, PlayerInfo.Position.Y) != TileContent.Lava)
+                    {
+                        moveLeft = true;
+                    }
+                    else
+                    {
+                        if (map.GetTileAt(PlayerInfo.Position.X, PlayerInfo.Position.Y - 1) != TileContent.Lava)
+                        {
+                            return AIHelper.CreateMoveAction(new Point(0, -1));
+                        }
+                        else if (map.GetTileAt(PlayerInfo.Position.X, PlayerInfo.Position.Y + 1) != TileContent.Lava)
+                        {
+                            return AIHelper.CreateMoveAction(new Point(0, 1));
+                        }
+                    }
+                }
 
-                if(nbMovesX > 0)
+                if (point.Y > 0)
                 {
-                    moveRight = true;
+                    if (map.GetTileAt(PlayerInfo.Position.X, PlayerInfo.Position.Y + 1) != TileContent.Lava)
+                    {
+                        moveDown = true;
+                    }
+                    else
+                    {
+                        if (map.GetTileAt(PlayerInfo.Position.X - 1, PlayerInfo.Position.Y) != TileContent.Lava)
+                        {
+                            return AIHelper.CreateMoveAction(new Point(-1, 0));
+                        }
+                        else if (map.GetTileAt(PlayerInfo.Position.X + 1, PlayerInfo.Position.Y) != TileContent.Lava)
+                        {
+                            return AIHelper.CreateMoveAction(new Point(1, 0));
+                        }
+                    }
                 }
-                else if(nbMovesX < 0)
+                else if (point.Y < 0)
                 {
-                    moveLeft = true;
-                }
-                if(nbMovesY > 0)
-                {
-                    moveDown = true;
-                }
-                else if(nbMovesY < 0)
-                {
-                    moveUp = true;
+                    if (map.GetTileAt(PlayerInfo.Position.X, PlayerInfo.Position.Y - 1) != TileContent.Lava)
+                    {
+                        moveUp = true;
+                    }
+                    else
+                    {
+                        if (map.GetTileAt(PlayerInfo.Position.X - 1, PlayerInfo.Position.Y) != TileContent.Lava)
+                        {
+                            return AIHelper.CreateMoveAction(new Point(-1, 0));
+                        }
+                        else if (map.GetTileAt(PlayerInfo.Position.X + 1, PlayerInfo.Position.Y) != TileContent.Lava)
+                        {
+                            return AIHelper.CreateMoveAction(new Point(1, 0));
+                        }
+                    }
                 }
 
                 Random random = new Random(DateTime.Now.Millisecond);
-                if(random.Next() % 2 == 0)
+                int nbRandom = random.Next() % 4;
+                if (nbRandom == 0)
                 {
                     if (moveLeft)
                     {
-                       return AIHelper.CreateMoveAction(new Point(-1, 0));
+                        return AIHelper.CreateMoveAction(new Point(-1, 0));
                     }
-                    else if(moveRight)
+                    else if (moveRight)
                     {
-                       return AIHelper.CreateMoveAction(new Point(1, 0));
+                        return AIHelper.CreateMoveAction(new Point(1, 0));
                     }
-                    else if(moveUp)
+                    else if (moveUp)
                     {
                         return AIHelper.CreateMoveAction(new Point(0, -1));
                     }
-                    else //(moveDown)
+                    else
                     {
                         return AIHelper.CreateMoveAction(new Point(0, 1));
                     }
                 }
-                else
+                else if(nbRandom == 1)
                 {
-                    if (moveUp)
+                    if(moveDown)
+                    {
+                        return AIHelper.CreateMoveAction(new Point(0, 1));
+                    }
+                    else if (moveLeft)
+                    {
+                        return AIHelper.CreateMoveAction(new Point(-1, 0));
+                    }
+                    else if (moveRight)
+                    {
+                        return AIHelper.CreateMoveAction(new Point(1, 0));
+                    }
+                    else
+                    {
+                        return AIHelper.CreateMoveAction(new Point(0, -1));
+                    }
+                }
+                else if (nbRandom == 2)
+                {
+                    if(moveUp)
                     {
                         return AIHelper.CreateMoveAction(new Point(0, -1));
                     }
@@ -197,87 +263,30 @@ namespace LHGames.Bot
                     {
                         return AIHelper.CreateMoveAction(new Point(-1, 0));
                     }
-                    else //(moveRight)
+                    else
+                    {
+                        return AIHelper.CreateMoveAction(new Point(1, 0));
+                    } 
+                }
+                else
+                {
+                    if(moveRight)
                     {
                         return AIHelper.CreateMoveAction(new Point(1, 0));
                     }
-                }
-            }
-
-            /// <summary>
-            /// Will create a table containing multiple MoveActions
-            /// Every MoveActions will be a direction point, 
-            /// ie: [1, 0], [1, 0], [0,-1] would represent the (right, right, up) path
-            /// </summary>
-            /// <param name="point"></param>
-            /// <returns></returns>
-            private static List<Point> FindPath(Map map, Point point)
-            {
-                // this list will contain all the moves to make to ge to the desired point
-                List<Point> path = new List<Point> { };
-                List<Point> emptyPoints = new List<Point> { }; // this list contains all the available points for moves
-                Point tempPosition;
-
-                foreach (Tile t in map.GetVisibleTiles())
-                {
-                    if (t.TileType != TileContent.Empty)
+                    else if (moveUp)
                     {
-                        emptyPoints.Add(t.Position);
+                        return AIHelper.CreateMoveAction(new Point(0, -1));
                     }
-                }
-
-                //A*
-                double flyingDistance = Math.Sqrt(Math.Pow((PlayerInfo.Position.X - point.X), 2) + Math.Pow((PlayerInfo.Position.Y - point.Y), 2));
-                int tileUnitsDistance = (PlayerInfo.Position.X - point.X) + (PlayerInfo.Position.Y - point.Y);
-
-                do
-                {
-                    tempPosition = new Point();
-                } while (tempPosition != point);
-
-                return path;
-            }
-
-            private static void AStarMoveUp()
-            {
-
-            }
-
-            private static void AStarMoveDown()
-            {
-
-            }
-            private static void AStarMoveLeft()
-            {
-
-            }
-            private static void AStarMoveRight()
-            {
-
-            }
-
-            /// <summary>
-            /// When called, the player will move in a specific direction
-            /// Input must be between [-1, -1] and [1, 1], and can only have 1 parameter != 0
-            /// </summary>
-            /// <param name="point">Move in the x axis. Left = [-1, 0], Right = [1, 0]
-            ///                     Move in the y axis. Top = [0, -1], Down = [0, 1] </param>
-            private static string Move(Point point)
-            {
-                if (point.X != 0 ^ point.Y != 0)
-                {
-                    if (point.X != 0) // move in x axis
+                    else if (moveDown)
                     {
-                        return AIHelper.CreateMoveAction(new Point(point.X, 0));
+                        return AIHelper.CreateMoveAction(new Point(0, 1));
                     }
-                    else // move in y axis
+                    else
                     {
-                        return AIHelper.CreateMoveAction(new Point(0, point.Y));
+                        return AIHelper.CreateMoveAction(new Point(-1, 0));
                     }
-                }
-                else // Called if the user sent inconsistent entrie values
-                {
-                    return AIHelper.CreateMoveAction(new Point(0, 0)); // Won't move
+                    
                 }
             }
         }
